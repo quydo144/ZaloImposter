faf51a9955bb9f4f6c96538118e26a1bf0c45a97
quydo144 <quydo144@gmail.com>
2020-12-11 18:47:12 +0700

    update adapter

---
+++ app/src/main/java/com/example/appchat/Adapter/Adapter_Nhom.java
@@ -0,0 +1,81 @@
+package com.example.appchat.Adapter;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.appchat.Models.NguoiDung;
+import com.example.appchat.R;
+
+import java.util.ArrayList;
+
+public class Adapter_Nhom extends RecyclerView.Adapter<Adapter_Nhom.MyViewHolderNhom> {
+    MyViewHolderNhom holderNhom;
+    Context context;
+    ArrayList<NguoiDung> itemNguoiDung;
+    ArrayList<NguoiDung> checkedNguoiDung= new ArrayList<>();
+    SharedPreferences preferences;
+    boolean statusBanBe;
+    OnMultiClickCheckBoxListener onListener;
+
+    public void setOnMultiClickCheckBoxListener(OnMultiClickCheckBoxListener onListener){
+        this.onListener= onListener;
+    }
+
+    public Adapter_Nhom (Context context, ArrayList<NguoiDung> lstNguoiDung, Boolean statusBanBe){
+        preferences= context.getSharedPreferences("data_dang_nhap", Context.MODE_PRIVATE);
+
+        this.context= context;
+        this.itemNguoiDung= lstNguoiDung;
+        this.statusBanBe= statusBanBe;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolderNhom onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView= LayoutInflater.from(context).inflate(R.layout.item_recycle_thembanchat, parent, false);
+        holderNhom = new MyViewHolderNhom(itemView, onListener);
+        return holderNhom;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolderNhom holder, int position) {
+        this.setOnMultiClickCheckBoxListener(new OnMultiClickCheckBoxListener() {
+            @Override
+            public void onMultiClickCheckBox(View view, int position) {
+                CheckBox chk= (CheckBox) view;
+
+                if (chk.isChecked()){
+                    checkedNguoiDung.add(itemNguoiDung.get(position));
+                } else if (!chk.isChecked()){
+                    checkedNguoiDung.remove(itemNguoiDung.get(position));
+                }
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return itemNguoiDung.size();
+    }
+
+    public class MyViewHolderNhom extends RecyclerView.ViewHolder implements View.OnClickListener{
+        CheckBox checkBoxThemBan;
+
+        public MyViewHolderNhom(@NonNull View itemView, OnMultiClickCheckBoxListener onListener) {
+            super(itemView);
+            this.checkBoxThemBan= itemView.findViewById(R.id.checkBox_themBanVaoNhom);
+        }
+
+        @Override
+        public void onClick(View v) {
+            onListener.onMultiClickCheckBox(v, getAdapterPosition());
+        }
+    }
+}
--- app/src/main/java/com/example/appchat/TaoNhomActivity.java
+++ app/src/main/java/com/example/appchat/TaoNhomActivity.java
@@ -1,18 +1,29 @@
 package com.example.appchat;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.appchat.Adapter.Adapter_Nhom;
+import com.example.appchat.Models.BanBe;
+import com.example.appchat.Models.Message;
 import com.example.appchat.Models.MessageNhom;
+import com.example.appchat.Models.NguoiDung;
 import com.example.appchat.Models.ThanhVien;
 import com.example.appchat.Retrofit2.APIUtils;
 import com.example.appchat.Retrofit2.DataClient;
+
+import java.util.ArrayList;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -24,12 +35,26 @@ public class TaoNhomActivity extends AppCompatActivity {
     TextView tenNhom;
     String idGroup = "";
     SharedPreferences preferences;
+    EditText txtSDT;
+    String SDT;
+    Adapter_Nhom adapter_nhom;
+    View view;
+    LinearLayoutManager layoutManager;
+    NguoiDung nguoi_dung_infor;
+    ArrayList<NguoiDung> lstUser = new ArrayList<>();
+    BanBe ban_be_info;
+    Integer status = -1;
+    RecyclerView recycleDSNhom_Tao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tao_nhom);
+        ban_be_info = new BanBe();
+        lstUser.clear();
         init_Data();
+        TextChange();
+        GetDanhSachBan();
         backFragmentNhom_Click();
     }
 
@@ -47,6 +72,72 @@ public class TaoNhomActivity extends AppCompatActivity {
         btnBackNhom = (ImageButton) findViewById(R.id.btnBack_FragmentNhom);
         btnDongYTaoNhom = (ImageButton) findViewById(R.id.btnXac_Nhan_Tao_Nhom);
         tenNhom = (TextView) findViewById(R.id.txtDatTenNhom);
+        SDT = preferences.getString("SoDienThoai", "");
+        recycleDSNhom_Tao = (RecyclerView) findViewById(R.id.recycleDSNhom_Tao);
+        layoutManager = new LinearLayoutManager(this);
+        recycleDSNhom_Tao.setLayoutManager(layoutManager);
+        txtSDT = findViewById(R.id.txtTimSoDienThoai);
+    }
+
+    protected void ShowDanhSach(){
+        adapter_nhom = new Adapter_Nhom(this, lstUser, false);
+
+        recycleDSNhom_Tao.setAdapter(adapter_nhom);
+        adapter_nhom.notifyDataSetChanged();
+    }
+
+    protected void GetDanhSachBan(){
+        int MaNguoiDung = preferences.getInt("MaNguoiDung" , 0);
+        DataClient client = APIUtils.getData();
+        BanBe banBe = new BanBe();
+        banBe.setMaNguoiDung_Mot(MaNguoiDung);
+        Call<Message> call = client.GetListFriend(banBe);
+        call.enqueue(new Callback<Message>() {
+            @Override
+            public void onResponse(Call<Message> call, Response<Message> response) {
+                if (response.isSuccessful()) {
+                    if (response.body().getSuccess() == 1) {
+                        for (NguoiDung user : response.body().getDanhsach()){
+                            if (user.isStatus()){
+                                lstUser.add(user);
+                            }
+                        }
+                        ShowDanhSach();
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Message> call, Throwable t) {
+
+            }
+        });
+    }
+
+    protected void TextChange() {
+        txtSDT.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                ArrayList<NguoiDung> lstUserTemp = new ArrayList<>();
+                for (NguoiDung user : lstUser) {
+                    if (user.getSoDienThoai().contains(s)) {
+                        lstUserTemp.add(user);
+                    }
+                }
+                adapter_nhom = new Adapter_Nhom(view.getContext(), lstUserTemp, false);
+                recycleDSNhom_Tao.setAdapter(adapter_nhom);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
     }
 
     private void CreateGroup(){
