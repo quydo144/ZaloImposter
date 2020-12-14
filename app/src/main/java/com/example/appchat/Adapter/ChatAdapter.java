fb13b4ccc47a1e8586bef90a7a85c6195da95c93
quydo144 <quydo144@gmail.com>
2020-12-14 15:59:13 +0700

    chat group

--- app/src/main/java/com/example/appchat/Adapter/ChatAdapter.java
+++ app/src/main/java/com/example/appchat/Adapter/ChatAdapter.java
@@ -1,16 +1,11 @@
 package com.example.appchat.Adapter;
 
-<<<<<<< HEAD
-=======
-import android.content.ClipData;
->>>>>>> 9c83b9d9793fd70885bbdaab25377bcf7165ddd1
 import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.cardview.widget.CardView;
@@ -26,22 +21,14 @@ import java.util.ArrayList;
 
 public class ChatAdapter extends RecyclerView.Adapter<ChatAdapter.ViewHolder> {
     private ArrayList<ConversationMap> Chats;
-<<<<<<< HEAD
 
     public ChatAdapter(ArrayList<ConversationMap> chats) {
-=======
-    public ChatAdapter(ArrayList<ConversationMap> chats){
->>>>>>> 9c83b9d9793fd70885bbdaab25377bcf7165ddd1
         this.Chats = chats;
     }
 
     private OnChatItemListener mListener;
 
-<<<<<<< HEAD
     public interface OnChatItemListener {
-=======
-    public interface OnChatItemListener{
->>>>>>> 9c83b9d9793fd70885bbdaab25377bcf7165ddd1
         void onChatItemClick(View v, int position);
     }
 
@@ -57,18 +44,10 @@ public class ChatAdapter extends RecyclerView.Adapter<ChatAdapter.ViewHolder> {
         if (Chats.get(position).getSender() != null) {
             holder.container_recylerview_item_user.setVisibility(View.VISIBLE);
             holder.tvTenNguoiNhan.setText(Chats.get(position).getSender().getHoTen());
-<<<<<<< HEAD
-=======
-
->>>>>>> 9c83b9d9793fd70885bbdaab25377bcf7165ddd1
             holder.setOnChatItemListener(new OnChatItemListener() {
                 @Override
                 public void onChatItemClick(View v, int position) {
                     NguoiDung nd = Chats.get(position).getSender();
-<<<<<<< HEAD
-=======
-
->>>>>>> 9c83b9d9793fd70885bbdaab25377bcf7165ddd1
                     Intent intent = new Intent(v.getContext(), NhanTinDonActivity.class);
                     intent.putExtra("sdt", nd.getSoDienThoai());
                     intent.putExtra("ten", nd.getHoTen());
@@ -76,12 +55,7 @@ public class ChatAdapter extends RecyclerView.Adapter<ChatAdapter.ViewHolder> {
                     v.getContext().startActivity(intent);
                 }
             });
-<<<<<<< HEAD
         } else {
-=======
-
-        }else {
->>>>>>> 9c83b9d9793fd70885bbdaab25377bcf7165ddd1
             holder.container_recylerview_item_user.setVisibility(View.GONE);
         }
     }
@@ -91,13 +65,7 @@ public class ChatAdapter extends RecyclerView.Adapter<ChatAdapter.ViewHolder> {
         return Chats.size();
     }
 
-<<<<<<< HEAD
     public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
-=======
-    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
-        OnChatItemListener onChatItemListener;
-
->>>>>>> 9c83b9d9793fd70885bbdaab25377bcf7165ddd1
         TextView tvTenNguoiNhan;
         CardView container_recylerview_item_user;
         OnChatItemListener onChatItemListener;
@@ -106,10 +74,6 @@ public class ChatAdapter extends RecyclerView.Adapter<ChatAdapter.ViewHolder> {
             super(itemView);
             tvTenNguoiNhan = (TextView) itemView.findViewById(R.id.nameUser);
             container_recylerview_item_user = (CardView) itemView.findViewById(R.id.container_recylerview_item_user);
-<<<<<<< HEAD
-=======
-
->>>>>>> 9c83b9d9793fd70885bbdaab25377bcf7165ddd1
             this.onChatItemListener = listener;
             itemView.setOnClickListener(this::onClick);
         }
